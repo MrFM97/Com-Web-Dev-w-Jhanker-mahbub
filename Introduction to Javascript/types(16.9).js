394db31b2cacc4b451b40ad3c96b1dac3eeae0ce
MrFM97 <shijawa@gmail.comm>
2022-07-31 05:43:38 +0600

    types, answer and remainder

---
+++ Introduction to Javascript/types(16.9).js
@@ -0,0 +1,30 @@
+var price = 100;
+console.log(typeof price);
+var price = '100';
+console.log(typeof price);
+var isHappy = true;
+console.log(typeof isHappy);
+var romantic
+console.log(typeof romantic);
+
+// Dynamic typing
+
+// Data types = 2 types - primitive and non-primitive
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(1);
+sum = parseFloat(sum);
+
+var mangoes = 7;
+var mangoes = 11;
+var person = 2;
+console.log(mangoes/person)
+//  this means vagfol
+
+// this means vagses
+var mangoes = 11;
+var person = 2;
+console.log(mangoes % person)
+
