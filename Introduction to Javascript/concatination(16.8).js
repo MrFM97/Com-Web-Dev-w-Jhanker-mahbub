ffcdedc8b53690765235411f9d66a926591af0f2
MrFM97 <shijawa@gmail.comm>
2022-07-31 05:29:58 +0600

    concat

---
+++ Introduction to Javascript/concatination(16.8).js
@@ -0,0 +1,24 @@
+var price1 = 31;
+var price2 = 10;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+var firstName = 'Jennie';
+var lastName = 'Kim';
+var fullNmae = firstName + ' ' + lastName;
+console.log(fullNmae);
+
+var price1 = '31';
+var price2 = '10';
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+//convert string to number
+
+console.log(price2);
+price1 = parseInt(price1);
+console.log(price1);
+
+var gpa = '3.41';
+var gpaNumber = parseFloat(gpa);
+console.log(gpa);
