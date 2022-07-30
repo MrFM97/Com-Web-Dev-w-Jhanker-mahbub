4cad13ec1dd46582d23c87bccc1edbe66de24406
MrFM97 <shijawa@gmail.comm>
2022-07-31 04:55:05 +0600

    addtiong, increment, decrement

---
+++ Introduction to Javascript/shorthand(16.7).js
@@ -0,0 +1,36 @@
+//variable declaration
+var TomatoPrice = 40;
+//variable value update
+TomatoPrice = 50;
+
+//add something to the previous value
+
+var price1 = 31;
+var price2 = 10;
+
+//set a new value
+price1 = 35;
+
+//add some value to the existing value
+price1 = price1 + 10;
+console.log(price1)
+
+// addition to the same variable shorthand
+// += adding some value to the existing value
+price1 += 10;
+console.log(price1)
+
+// price2 = price2 -5;
+price2 -= 5;
+console.log(price1)
+
+// *=
+price1 *= 5;
+console.log(price1)
+
+// /= 
+price1 /= 5;
+console.log(price1)
+
+//  ++ increment (adding 1 to existing value)
+// -- decrement (deducting 1 to existing value)
