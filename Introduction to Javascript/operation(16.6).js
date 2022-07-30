3f68d4f316559a91c0f64bc99e3832d6aa650da0
MrFM97 <shijawa@gmail.comm>
2022-07-31 04:39:42 +0600

    operator practice

---
+++ Introduction to Javascript/operation(16.6).js
@@ -0,0 +1,22 @@
+var onionPrice = 20;
+var eggPrcie = 10;
+// console.log(onionPrice)
+// console.log(eggPrcie);
+// console.log(onionPrice + eggPrcie)
+
+//addition and substraction
+var TotalPrice = onionPrice + eggPrcie;
+console.log(TotalPrice)
+
+var priceDifference = onionPrice - eggPrcie;
+console.log(priceDifference);
+
+var orangePrice = 20;
+var quantity = 7;
+var totalCost = orangePrice * quantity;
+console.log(totalCost);
+
+var potatoPrice = 100;
+var quantity = 5;
+var kiloPerCost = potatoPrice / quantity;
+console.log(kiloPerCost);
