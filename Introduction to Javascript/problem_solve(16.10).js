5527b481e5984a2c248d15e30f83f3fb60461d3c
MrFM97 <shijawa@gmail.comm>
2022-07-31 17:28:11 +0600

    All problem solved

---
+++ Introduction to Javascript/problem_solve(16.10).js
@@ -0,0 +1,31 @@
+// Practice problem-1
+
+var motherMoney = 500;
+var fruits = 200;
+var shopkeeprsReturn = motherMoney - fruits;
+console.log(shopkeeprsReturn);
+
+// Practice problem -2
+
+var math = 70;
+var biology = 75;
+var chemistry = 65;
+var physics = 75;
+var bangla = 80;
+
+var addition = math + biology + chemistry + physics + bangla;
+var avg = parseFloat(addition / 5).toFixed(2);
+console.log(avg) 
+
+// Practice Problem -3
+
+var firstLine = "I am feeling confident";
+var secondLine = "I'll be an awesome web developer";
+var oneLine = firstLine + ' ' + secondLine;
+console.log(oneLine);
+
+// Practice Problem -4
+
+var numberGiven = 69;
+var divided = 5;
+console.log(numberGiven % divided);
