b48d391f8be302e24ec700493c6820ca94f426fc
MrFM97 <shijawa@gmail.comm>
2022-07-31 23:28:14 +0600

    basic array introduction

---
+++ Array and conditionals(Module-17)/array(17.2).js
@@ -0,0 +1,6 @@
+var friendsAge = [11, 21, 45, 17, 14, 56, 06];
+var picnicFee = [5000, 2000, 4000, 150];
+var nayikas = ["mahi", "opu", "shabnoor", 'sabana'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(friendsAge.length);
