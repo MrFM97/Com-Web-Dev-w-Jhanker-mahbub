19c31ee293ae8e94f61822f86b860bd945a9c8a9
MrFM97 <shijawa@gmail.comm>
2022-07-31 23:42:02 +0600

    array index added

---
+++ Array and conditionals(Module-17)/arrayIndex(17.3).js
@@ -0,0 +1,14 @@
+var numbers = [45, 68, 89, 98, 56, 78];
+
+//get element by index
+var second = numbers[1];
+console.log(second);
+
+//set element value by index
+numbers[1] = 77;
+numbers[3] = 44;
+console.log(numbers);
+
+//find index of an element
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex)
