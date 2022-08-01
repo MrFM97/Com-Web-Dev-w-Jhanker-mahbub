6dff181d80d502c8e43517db6318848b47407c2e
MrFM97 <shijawa@gmail.comm>
2022-08-01 17:17:11 +0600

    if else added

---
+++ Array and conditionals(Module-17)/conditional.js
@@ -0,0 +1,18 @@
+var iphonePrice = 79000;
+var myBudget = 9500;
+
+//if iphone price is less than my budget. I will buy the iphone 
+
+/* if iphone price is less than my budget 
+I will buy the phone 
+
+
+*/
+
+if (iphonePrice < myBudget) {
+    console.log('iphone kine futani marbo');
+}
+
+else{
+    console.log('xiaomi phone kinbo');
+}
