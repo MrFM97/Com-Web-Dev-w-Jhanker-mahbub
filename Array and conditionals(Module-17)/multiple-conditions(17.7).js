d065758a1aa69342d13970d6d3e11c6acd494de0
MrFM97 <shijawa@gmail.comm>
2022-08-01 19:32:46 +0600

    MC end

---
+++ Array and conditionals(Module-17)/multiple-conditions(17.7).js
@@ -0,0 +1,38 @@
+/* var isGraduated = true;
+var salary = 25000; */
+
+/* if(isGraduated == true){
+    console.log('cholo biye kore feli, ki r ase jibon e');
+}
+
+else{
+    console.log('Amar kopal e biye nai ');
+} */
+
+/* if(isGraduated == true && salary > 50000 && cars >=1){
+    console.log('Eso biye kore feli');
+}
+
+else{
+    console.log('Amar kopal e biye nai'); 
+} */
+
+var isGraduated = true;
+var salary = 47000;
+var cars = 1;
+
+if(isGraduated === true || salary > 50000 || cars >=1){
+    console.log('Eso Prem Kori');
+}
+
+else{
+    console.log('tomar friend er mobile number dao');
+}
+
+if(isGraduated === true && salary > 50000 && cars >=1){
+    console.log('Eso Prem Kori');
+}
+
+else{
+    console.log('tomar friend er mobile number dao');
+}
