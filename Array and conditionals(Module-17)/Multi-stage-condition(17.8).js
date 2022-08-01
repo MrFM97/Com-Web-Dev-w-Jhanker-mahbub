d71df9a68424644ab5cb4d96b08b5cb285ae0dc1
MrFM97 <shijawa@gmail.comm>
2022-08-01 19:56:09 +0600

    if else

---
+++ Array and conditionals(Module-17)/Multi-stage-condition(17.8).js
@@ -0,0 +1,20 @@
+var money = 44;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if (danishPrice < money) {
+    console.log('Dan Dan danish khamu');
+}
+
+else if (butterBread < money) {
+    console.log('butter bread khamu');
+}
+
+else if(toastBiscuit < money) {
+    console.log('toast khabo');
+}
+
+else{
+    console.log('gorib er cha e vorsa');
+}
