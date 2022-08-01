b390b6f9854b1b782e0bad3505e01c2d733cce5f
MrFM97 <shijawa@gmail.comm>
2022-08-01 20:12:44 +0600

    nested if

---
+++ Array and conditionals(Module-17)/nested-conditions(17.8).js
@@ -0,0 +1,15 @@
+//checking if you are good at math or not
+var math = true;
+var geometry = true;
+
+if (math == true) {
+    if (geometry == true) {
+        console.log('pro af!');
+    }
+    else {
+        console.log('etodin ki shikhso?');
+    }
+}
+else {
+    console.log('noob like me');
+}
