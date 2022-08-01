cdc966e77b7d19fc3b8719e6a805bdb8fd132b9a
MrFM97 <shijawa@gmail.comm>
2022-08-01 17:56:56 +0600

    conditional name changed and multiple conditions added

---
+++ Array and conditionals(Module-17)/conditional(17.6).js
@@ -0,0 +1,42 @@
+var iphonePrice = 79000;
+var myBudget = 9500;
+
+//if iphone price is less than my budget. I will buy the iphone 
+
+/* if iphone price is less than my budget 
+I will buy the phone 
+
+
+*/
+
+if (iphonePrice < myBudget) {
+    console.log('iphone kine futani marbo');
+}
+
+else{
+    console.log('xiaomi phone kinbo');
+}
+
+if (iphonePrice > myBudget) {
+    console.log('iphone na kine basay jabo na');
+}
+else{
+    console.log('sad loip');
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+if (chickenPrice >= myMoney) {
+    console.log('murgi kheye foorti kori');
+}
+else{
+    console.log('bhat bhorta e bhorsa');
+}
+
+if (chickenPrice < myMoney) {
+    console.log('chicken fry khabo');
+}
+
+else{
+    console.log('Cha-Pani khabo');
+}
