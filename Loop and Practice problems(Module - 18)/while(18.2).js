1048f1bbfd5c1e877c8744fecf2e6f70c87c2302
MrFM97 <shijawa@gmail.comm>
2022-08-02 18:05:04 +0600

    basic while loop stated

---
+++ Loop and Practice problems(Module - 18)/while(18.2).js
@@ -0,0 +1,21 @@
+/* if(condition){
+    
+} */
+
+    // roastGiven = roastGiven +1;
+    // roastGiven += 1;
+
+/*
+1. Loop variable
+2. Condition inside while
+3. Loop Body
+4. change the loop variable
+*/
+
+var roastGiven = 0;
+while(roastGiven < 7) {
+    console.log('Roast Den, Please !!');
+    console.log('roastGiven');
+    // roastGiven++;
+    roastGiven = roastGiven +1;
+}
