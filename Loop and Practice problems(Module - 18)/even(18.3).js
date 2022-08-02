db71c74cf4882410f91ebcf30d13de4ccba66529
MrFM97 <shijawa@gmail.comm>
2022-08-02 18:22:14 +0600

    even adn odd number added

---
+++ Loop and Practice problems(Module - 18)/even(18.3).js
@@ -0,0 +1,13 @@
+var number = 2;
+while(number <= 10){
+    console.log(number);
+    number= number + 2;
+}
+
+//odd numbers
+
+var number = 1;
+while(number <= 10){
+    console.log(number);
+    number= number + 2;
+}
