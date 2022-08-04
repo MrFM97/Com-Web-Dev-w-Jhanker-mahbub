538ff1e44d022b54fe764bb14e100ec19fd9dc70
MrFM97 <shijawa@gmail.comm>
2022-08-04 15:53:54 +0600

    parameter

--- Core concepts, functions and objects(Module -19)/parameter.js
+++ Core concepts, functions and objects(Module -19)/parameter.js
@@ -1,6 +1,19 @@
 function bringSingara(money) {
     console.log('mama singara den');
-    console.log(money);
+    console.log('Eto taka disen: ', money);
+}
+// bringSingara(100);
+
+function add(num1, num2) {
+    console.log('going to add:', num1, num2);
 }
 
-bringSingara(300);
+// add(125, 69);
+
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(15, 16, 98)
