287d69523deaedd904400cd72e0dd8bba140c78b
MrFM97 <shijawa@gmail.comm>
2022-08-04 12:23:50 +0600

    function and parameter

---
+++ Core concepts, functions and objects(Module -19)/function.js
@@ -0,0 +1,17 @@
+// function declaration
+function startFan() {
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('Press the switch');
+    
+}
+
+//call the function
+startFan();
+console.log('waking up in the morning');
+startFan(); 
+console.log('start the fan');
+startFan();
+console.log('watching JS tutorial');
+startFan();
+console.log('Jhanker mahbub is great');
