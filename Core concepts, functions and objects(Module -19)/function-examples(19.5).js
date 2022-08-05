9b68bf63a54a9158bb6147c658d49aa8ee409478
MrFM97 <shijawa@gmail.comm>
2022-08-05 17:43:10 +0600

    object

--- Core concepts, functions and objects(Module -19)/function-examples(19.5).js
+++ Core concepts, functions and objects(Module -19)/function-examples(19.5).js
@@ -5,7 +5,7 @@
 
 var returnedValue = functionName(parameters value) */
 
-function getAverage(assignment1, assignment2, assignment3) {
+/* function getAverage(assignment1, assignment2, assignment3) {
     const total = assignment1 + assignment2 + assignment3;
     const average = total / 3;
     return average;
@@ -16,4 +16,11 @@ const assignment2Marks = 58;
 const assignment3Marks = 59;
 
 var myAverage = getAverage(assignment1Marks, assignment2Marks, assignment3Marks);
-console.log('my average so far: ', myAverage);
+console.log('my average so far: ', myAverage); */
+
+function movie(){
+    return "Din-The day";
+    return "Poran";
+    return "Hawa";
+  }
+  console.log(movie());
---
+++ Core concepts, functions and objects(Module -19)/object(19.6).js
@@ -0,0 +1,26 @@
+var student = { 
+    id: 115, 
+    name: 'mubtasim murta', 
+    class: 9, 
+    marks: 98
+}
+
+var mobile = {
+    brand: 'Realme',
+    price: 23000,
+    Storage: '128gb',
+    camera: '64MP'
+}
+// brand,price - properties
+// realme, 23000, - values
+
+var myComputer = {
+    brand: 'Walton',
+    price: 52000,
+    color: 'black',
+    processor: 'i7'
+}
+
+console.log(myComputer);
+myComputer.processor = 'i79';
+console.log(myComputer);
