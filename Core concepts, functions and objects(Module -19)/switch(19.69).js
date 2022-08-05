b1e508364653dff9d1ac2779ca13638ad4cac593
MrFM97 <shijawa@gmail.comm>
2022-08-05 21:16:48 +0600

    switch statement

---
+++ Core concepts, functions and objects(Module -19)/switch(19.69).js
@@ -0,0 +1,41 @@
+const color = 'green';
+if (color === 'green') {
+    console.log('You got a green friend');
+}
+else if (color === 'blue'){
+    console.log('you are my blue friend');
+}
+else if (color === 'red'){
+    console.log('you are my red friend');
+}
+else if (color === 'white'){
+    console.log('you are my white friend');
+}
+else if (color === 'yellow'){
+    console.log('you are my yellow himu friend');
+}
+else if (color === 'black'){
+    console.log('you are my black friend');
+}
+
+// switch
+switch (color) {
+    case 'green':
+        console.log('you are my green friend');
+        break;
+
+    case 'blue':
+        console.log('you got a blue friend');
+        break;
+    case 'white':
+        console.log('you are my white friend');
+        break;
+    case 'red':
+        console.log('you are my red friend');
+        break;
+    case 'yellow':
+        console.log('you are my yellow friend');
+        break;
+    default:
+        console.log('you are a kala kala friend');
+}
