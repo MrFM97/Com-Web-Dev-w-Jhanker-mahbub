2e4da72cb74992a28ce4b133c6d61a0896a26e56
MrFM97 <shijawa@gmail.comm>
2022-08-08 23:42:14 +0600

    min and max problem solved

---
+++ JS simple coding problems(Module - 21)/max(21.6).js
@@ -0,0 +1,41 @@
+/* const jim = 84;
+const dela = 75;
+if(jim < dela) {
+    console.log('Jim will get the cake');
+}
+
+else{
+    console.log('Dela you will get the cake');
+} */
+
+//Jim, chinku and dela
+/* const jim = 98;
+const dela = 90;
+const chinku = 97;
+
+if(jim > dela && jim > chinku){
+    console.log('jim will get the cake');
+}
+else if (dela > jim && dela > chinku){
+    console.log('Dela will get the cake');
+}
+else{
+    console.log('Chinku will get the cake');
+} */
+
+// easiest way - Math.min / Math.max
+
+/* const jim = 98;
+const dela = 90;
+const chinku = 97;
+
+const cake = Math.min(jim, dela, chinku);
+console.log(cake); */
+
+const jim = 90;
+const dela = 89;
+const chinku = 97;
+
+const cake = Math.max(jim, dela, chinku);
+console.log(cake);
+
--- JS simple coding problems(Module - 21)/tempCodeRunnerFile.js
+++ JS simple coding problems(Module - 21)/tempCodeRunnerFile.js
@@ -1,1 +1,6 @@
-a
+const jim = 98;
+const dela = 90;
+const chinku = 97;
+
+const cake = Math.min(jim, dela, chinku);
+console.log(cake);
