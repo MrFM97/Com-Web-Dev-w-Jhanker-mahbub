d5707f4903f391cf82eb1e920c4f0e08ab020a13
MrFM97 <shijawa@gmail.comm>
2022-08-08 22:31:53 +0600

    math

--- JS simple coding problems(Module - 21)/math(21.4).js
+++ JS simple coding problems(Module - 21)/math(21.4).js
@@ -1,3 +1,31 @@
 const result = Math.pow(3, 8);
 console.log(result);
 
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+
+if(gap < 5){
+    console.log('you guys can be friends');
+}
+else{
+    console.log('you guys stay apart');
+}
+
+// ----------------------------
+
+// ----------------------------
+
+const number = 2.45698;
+const fullNumber = Math.round(number);
+// console.log(fullNumber);
+const result2 = Math.ceil(2.001);
+const result3 = Math.floor(2.001);
+
+
+// random
+
+console.log(Math.random());
+const random = Math.round(Math.random()*100);
+console.log(random);
