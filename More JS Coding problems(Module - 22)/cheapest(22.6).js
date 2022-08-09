8fe4e4abb5570cc8307a86456e4cf6ba58c6a395
MrFM97 <shijawa@gmail.comm>
2022-08-10 02:14:14 +0600

    moddho rat er push

---
+++ More JS Coding problems(Module - 22)/cheapest(22.6).js
@@ -0,0 +1,9 @@
+const phones = [
+    {name: 'Realme', camera: 64, storage: '128gb', price: 23000, color: 'blue'},
+    {name: 'Asus', camera: 16, storage: '16gb', price: 10000, color: 'black'},
+    {name: 'iphone', camera: 12, storage: '256gb', price: 87000, color: 'silver'},
+    {name: 'xiaomi', camera: 32, storage: '64gb', price: 24000, color: 'black'},
+    {name: 'oppo', camera: 50, storage: '64gb', price: 20000, color: 'purple'},
+    {name: 'Nokia', camera: 10, storage: '32gb', price: 34000, color: 'white'},
+    {name: 'HTC', camera: 70, storage: '64gb', price: 40000, color: 'maroon'},
+];
