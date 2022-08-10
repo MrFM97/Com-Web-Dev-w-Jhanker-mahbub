407935d331ff00033ab1fb347016e51de1f86301
MrFM97 <shijawa@gmail.comm>
2022-08-11 03:34:17 +0600

    last push for tonight

---
+++ Basic JS Assignment (Module - 23)/assignment-4.js
@@ -0,0 +1,75 @@
+// Problem number - 1 (Radian to Degree)
+
+function radianToDegree(radian) {
+    if (typeof radian !== 'number') {
+        return 'Error!! Please Enter a Number';
+    } else {
+        const degree = radian * (180 / Math.PI);
+        return degree.toFixed(2);
+    }
+}
+
+//  Problem number - 2 (isJavaScriptFile)
+
+function isJavaScriptFile(fileName) {
+    if (typeof fileName !== 'string') {
+        return 'Beware, Please Enter a string!';
+    }
+    else {
+        if (fileName.endsWith('.js')) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+
+
+// // Problem number - 3 (oilPrice)
+
+function oilPrice(params) {
+
+}
+
+// Problem number - 4 (publicBusFare)
+function publicBusFare(public) {
+    const reservedBusSeats = 50;
+    const microBusSeats = 11;
+    const publicBusFare = 250;
+    if (typeof public === "number") {
+        if (public < 50) {
+            const divisible = parseInt(public / microBusSeats);
+            const publicLeft = public - microBusSeats * divisible;
+            const busFare = publicBusFare * publicLeft;
+            return busFare;
+        } else {
+            const divisibleByReservedBus = parseInt(public / reservedBusSeats);
+            let publicLeft = public - reservedBusSeats * divisibleByReservedBus;
+            const divisibleByMicroBus = parseInt(publicLeft / microBusSeats);
+            publicLeft -= microBusSeats * divisibleByMicroBus;
+            const busFare = publicBusFare * publicLeft;
+            return busFare;
+        }
+    } else {
+        return "Error, Please provide a Number";
+    }
+}
+
+// Problem number - 5 (isBestFriend)
+
+function isBestFriend(bef1, bef2) {
+    if (typeof bef1 !== 'object' && typeof bef2 !== 'object') {
+        return 'Error!! Please Enter Object. Not sentence or number';
+    }
+    else {
+        if (bef1.name === bef2.friend && bef1.friend === bef2.name) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
