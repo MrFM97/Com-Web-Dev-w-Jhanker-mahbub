0992b4fb290a773a4f7e8ffaf1f37fd87c5b5ad3
MrFM97 <shijawa@gmail.comm>
2022-08-12 03:50:49 +0600

    problem files

---
+++ Core concepts, functions and objects(Module -19)/problem solving/multiply-table.js
@@ -0,0 +1,10 @@
+// একটা ফাংশন লিখবা যেটা ১৩ এর নামতা (multiplication table) আউটপুট হিসেবে দেখাবে ।
+
+function thirteen(table) {
+
+    for (let i = 1; i <= 10; i++) {
+        console.log(i * table);
+    }
+
+}
+thirteen(13)
