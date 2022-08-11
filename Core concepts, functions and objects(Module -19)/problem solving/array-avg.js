91cc69917c38e93f27265e9838a2cf45a11332cf
MrFM97 <shijawa@gmail.comm>
2022-08-12 04:15:32 +0600

    another problem solved

--- Core concepts, functions and objects(Module -19)/problem solving/array-avg.js
+++ Core concepts, functions and objects(Module -19)/problem solving/array-avg.js
@@ -1,3 +1,12 @@
+/* Write a function called make_avg() which will take an array of
+integers and the size of that array and return the average of those values. */
+
 function make_avg(pera) {
-    
+    let total = 0;
+        for(i = 0; i < Array.length; i++){
+            total = total + array[i];
+        }
+        console.log(total / array.length);
 }
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+make_avg(myArray);
