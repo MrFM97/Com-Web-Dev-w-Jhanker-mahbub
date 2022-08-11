a90fe0d418281729a392a158ac60990c4bf9f48f
MrFM97 <shijawa@gmail.comm>
2022-08-12 01:06:52 +0600

    js checklist updated

---
+++ JS concept recap(Module -20.5)/Practice Problems/c2f.js
@@ -0,0 +1,7 @@
+function c2f(celcius) {
+
+    return celcius + ' is ' + celcius * 9 / 5 + 32 + ' Fahrenheit';
+}
+
+const output = c2f(56);
+console.log(output);
---
+++ JS concept recap(Module -20.5)/Practice Problems/f2c.js
@@ -0,0 +1,8 @@
+function fToC(fahrenheit) 
+{
+    return (fahrenheit - 32) * 5 / 9;
+
+}
+
+const output = fToC(32);
+console.log(output);
