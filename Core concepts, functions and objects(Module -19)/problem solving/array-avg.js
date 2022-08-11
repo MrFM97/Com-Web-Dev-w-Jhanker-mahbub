84fe1ccf8d7f2fb73fb1fb9c567e15fa3a06237f
MrFM97 <shijawa@gmail.comm>
2022-08-12 04:20:57 +0600

    array avg

--- Core concepts, functions and objects(Module -19)/problem solving/array-avg.js
+++ Core concepts, functions and objects(Module -19)/problem solving/array-avg.js
@@ -10,3 +10,9 @@ function make_avg(pera) {
 }
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 make_avg(myArray);
+
+/* for (arr of pera) {
+    if (arr == 69) {
+        console.log(arr);
+    }
+} */
---
+++ Core concepts, functions and objects(Module -19)/problem solving/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+function make_avg(pera) {
+    let total = 0;
+        for(i = 0; i < Array.length; i++){
+            total = total + array[i];
+        }
+        console.log(total / array.length);
+}
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+make_avg(myArray);
