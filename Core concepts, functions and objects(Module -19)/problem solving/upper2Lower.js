c5430cf2c020bdc024f8b4a443c860e95171ab4f
MrFM97 <shijawa@gmail.comm>
2022-08-12 19:58:08 +0600

    u2l

---
+++ Core concepts, functions and objects(Module -19)/problem solving/upper2Lower.js
@@ -0,0 +1,8 @@
+/* একটা ফাংশন লিখবা যে টা যেকোন নামকে uppercase বা রেগুলার কেইস হিসেবে নিবে আর
+আউটপুট হিসেবে সেই নাম lowercase করে রিটার্ন করবে । */
+
+function upperCase() {
+    let text = "Hello World!";
+    let result = text.toLowerCase();
+}
+console.log(result);
