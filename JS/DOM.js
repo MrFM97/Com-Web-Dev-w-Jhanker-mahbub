df6b63fc5faa66daf6f1f6300e1a56818a53035f
MrFM97 <shijawa@gmail.comm>
2022-08-15 02:57:05 +0600

    CSS in JS

--- JS/DOM.js
+++ JS/DOM.js
@@ -1,2 +1,16 @@
-console.log('Hello from JS');
-console.log(Document);
+const sections = document.querySelectorAll('section');
+for (const section of sections) {
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '15px';
+    section.style.paddingLeft = '7px';
+    section.style.backgroundColor = 'grey';
+
+}
+
+/* const placesContainer = document.getElementById('places-container');
+placesContainer.style.backgroundColor = 'teal'; */
+
+const placesContainer = document.getElementById('places-container');
+placesContainer.classList.add('text-center');
+placesContainer.classList.remove('large-text');
