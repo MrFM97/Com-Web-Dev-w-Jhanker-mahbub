1d03b6caef5f82c855354a62fb1e37cc58ddae5b
MrFM97 <shijawa@gmail.comm>
2022-08-15 20:57:38 +0600

    3 option described

--- Event, addEventListener, Event bubble (module - 25)/js/events.js
+++ Event, addEventListener, Event bubble (module - 25)/js/events.js
@@ -1,1 +1,22 @@
-console.log('this is event');
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// add onclick function
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+// make-pink grabber
+const makePinkButton = document.getElementById('make-pink');
+makePinkButton.onclick = makePink;
+    function makePink(){
+        document.body.style.backgroundColor = 'pink';
+    }
+
+// purple grabber
+const makePurpleButton = document.getElementById('make-pink');
+makePurpleButton.onclick = makePurple;
+    function makePurple(){
+        document.body.style.backgroundColor = 'purple';
+    }
