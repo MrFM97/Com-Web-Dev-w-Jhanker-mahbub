679b4290a446da8cd82585e9771e343fd94d69b4
MrFM97 <shijawa@gmail.comm>
2022-08-15 06:02:30 +0600

    module 24 renamedd

---
+++ Tour of DOM (module - 24)/JS/DOM.js
@@ -0,0 +1,17 @@
+const sections = document.querySelectorAll('section');
+for (const section of sections) {
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '15px';
+    section.style.paddingLeft = '7px';
+    section.style.backgroundColor = 'grey';
+
+}
+
+/* const placesContainer = document.getElementById('places-container');
+placesContainer.style.backgroundColor = 'teal'; */
+
+const placesContainer = document.getElementById('places-container');
+placesContainer.classList.add('yellow-bg');
+// placesContainer.classList.add('text-center');
+// placesContainer.classList.remove('large-text');
