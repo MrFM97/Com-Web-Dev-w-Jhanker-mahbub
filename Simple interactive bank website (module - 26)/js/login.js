b07675cca9441eb6ae1cd4df036220d4d7045251
MrFM97 <shijawa@gmail.comm>
2022-08-17 17:48:13 +0600

    id added, event listener added for user validity

---
+++ Simple interactive bank website (module - 26)/js/login.js
@@ -0,0 +1,24 @@
+// 1. click handler button added with submit button    
+document.getElementById('btn-submit').addEventListener('click', function() {
+    // 2. get the email address inside the email input field
+    // have remember to use .value to get text from an input field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    // 3. get a password
+    // 3.(a): set an id on the html element 
+    // 3.(b): get the element
+    // 3.(c): get the value from the element
+
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(email, password);
+
+    // Danger: do not varify email password in this way all the time (ideal way would be taught later)
+    // 4. varify email and password and check about valid user and invalid user 
+    if (email === 'shijawa@gmail.com' && password === 'secretSuperstar') {
+        console.log('valid user');
+    }
+    else{
+        console.log('invalid user');
+    }
+})
