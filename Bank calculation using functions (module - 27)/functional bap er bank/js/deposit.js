7ee7df81bd27c6cc1839d362c761bbb8c29da0b3
MrFM97 <shijawa@gmail.comm>
2022-08-19 17:47:18 +0600

    share on function between two input field

--- Bank calculation using functions (module - 27)/functional bap er bank/js/deposit.js
+++ Bank calculation using functions (module - 27)/functional bap er bank/js/deposit.js
@@ -1,3 +1,17 @@
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputField;
+}
+
+function getElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
+    return value;
+}
 document.getElementById('btn-deposit').addEventListener('click', function() {
-    
+    const newDepositAmount = getInputFieldValueById('deposit-field');
 })
