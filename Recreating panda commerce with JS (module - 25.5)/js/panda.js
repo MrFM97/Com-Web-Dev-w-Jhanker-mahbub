6c720a7c125a5c2ce5ebcd95c0594059e3f22d39
MrFM97 <shijawa@gmail.comm>
2022-08-20 17:54:59 +0600

    4 problem solved

--- Recreating panda commerce with JS (module - 25.5)/js/panda.js
+++ Recreating panda commerce with JS (module - 25.5)/js/panda.js
@@ -1,4 +1,21 @@
+// যতগুলা ইলিমেন্টে h2 ট্যাগ আছে সেগুলাকে সিলেক্ট করে তার লেখাগুলার কালার lightblue করেদিতে পারো কিনা। 
 const element = document.getElementsByTagName("h2");
 for (const h2 of element) {
     h2.style.color = 'lightblue';
 }
+
+// ওই ওয়েবসাইট এ backpack আইডি দিয়ে একটা সেকশন আছে। সেই সেকশনের ব্যাকগ্রাউন্ড কালার tomato করে দিতে পারো কিনা। 
+document.getElementById("bags").style.backgroundColor = "tomato";
+
+// card ক্লাস যতগুলা জায়গায় ইউজ করা হয়েছে সবগুলা কার্ড এর বর্ডার রেডিয়াস ৩০px করে দিতে পারো কিনা 
+document.getElementById('shoes').style.borderRadius = '30px';
+document.getElementById('bags').style.borderRadius = '30px';
+
+// কোন একটা ফাংশন লিখো। সেটার ভিতরে কনসোল লগ হবে। এবং সেই ফাংশন টা ক্লিক হ্যান্ডলার হিসেবে কোন একটা বাটনে যোগ করো 
+const funButton = document.querySelector(".panda-btn-warning");
+funButton.addEventListener('click', pandaButton);
+function pandaButton() {
+    console.log('button clicked');
+}
+
+/* এইবার নতুন করে সবগুলা buy now বাটনে এমন একটা ইভেন্ট হ্যান্ডলার যোগ করো। যাতে যেকোন একটা buy now বাটনে চাপ দিলে সেটা ওয়েবসাইট থেকে রিমুভ হয়ে যায়। একটু চিন্তা করে করার চেষ্টা করো।  */
