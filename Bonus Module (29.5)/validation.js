d2bb6ed12959ffa67170d3c696008d8fa976c76d
MrFM97 <shijawa@gmail.comm>
2022-08-23 21:09:44 +0600

    last commit of this milestone

---
+++ Bonus Module (29.5)/validation.js
@@ -0,0 +1,14 @@
+const number = '23';
+if (typeof number === 'number') {
+    console.log('value is a number');
+}
+else{
+    console.log('value is not a number');
+}
+// String, number, boolean
+
+const numbers = [3, 12, 78, 90];
+const student = {name: 'rongila rafsan', job: 'khawadaoa kora'}
+// console.log(Array.isArray(numbers));
+console.log(Array.isArray(student));
+console.log(typeof student);
