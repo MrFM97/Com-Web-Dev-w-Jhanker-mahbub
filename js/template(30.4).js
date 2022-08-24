2dfee9704d00edaf69ea5de3a247feefde9d0010
MrFM97 <shijawa@gmail.comm>
2022-08-24 20:23:36 +0600

    ES6 started

---
+++ js/template(30.4).js
@@ -0,0 +1,18 @@
+const person = 'Adam Sandler';
+const person2 = 'Ben White';
+const person3 = `Donald Trump`;
+
+const newMultiline = `First Line of text
+Second line of text
+third line of string
+fourth line of code`
+
+console.log(newMultiline);
+
+const a = 20;
+const b = 30;
+const summery = 'sum of: ' + a + 'and' + b + 'is' + (a+b);
+// console.log(summery);
+
+const newSummery = `sum of ${a} and ${b} is: ${a+b}`;
+console.log(newSummery);
