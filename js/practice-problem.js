be1864af88d306b6d12bce115ef15747a4d11f79
MrFM97 <shijawa@gmail.comm>
2022-08-25 17:58:41 +0600

    problem

---
+++ js/practice-problem.js
@@ -0,0 +1,25 @@
+/* Write an arrow function that will take 3 parameters, will multiply
+the parameters and will return the result. */
+
+const add = (first, second, third) => first + second + third;
+
+const result = add(10, 20, 50);
+console.log(result);
+
+/* Write the following sentence in three lines and print the result:
+I am a web developer. I love to code. I love to eat biryani. */
+
+const webDeveloper = `
+I am a web developer. 
+I love to code. 
+I love to eat biryani.`
+
+console.log(webDeveloper);
+
+/* Write an arrow function that will take 2 parameters: One parameter
+will come from you and the other parameter will be a default
+parameter. Add these two parameters and return the result. */
+
+const func = (param1, param2) => {
+    
+}
