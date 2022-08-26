5ec9cb6d9d46ef511be6ce8dc3bf62102496e958
MrFM97 <shijawa@gmail.comm>
2022-08-27 04:36:02 +0600

    more data, more API

--- API,JSON,Data load,dynamic website (Module - 32)/js/fetch(32.3).js
+++ API,JSON,Data load,dynamic website (Module - 32)/js/fetch(32.3).js
@@ -2,11 +2,11 @@
   .then(response => response.json())
   .then(json => console.log(json)) */
 
-  /* fetch(url)
-  .then(response => response.json())
-  .then(json => console.log(json);) */
-  
-  function loadData() {
+/* fetch(url)
+.then(response => response.json())
+.then(json => console.log(json);) */
+
+function loadData() {
     const url = 'https://jsonplaceholder.typicode.com/todos/1';
     fetch(url)
         .then(response => response.json())
