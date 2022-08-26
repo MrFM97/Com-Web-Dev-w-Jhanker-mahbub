e5cd261f63e1d104b16f5c1ce699d9ce843346ff
MrFM97 <shijawa@gmail.comm>
2022-08-26 10:23:04 +0600

    map problem solved

--- ES6, Class, Inheritance (Module - 31)/map(31.3).js
+++ ES6, Class, Inheritance (Module - 31)/map(31.3).js
@@ -1,6 +1,6 @@
-/* const numbers = [2, 8, 4, 6, 3];
+const numbers = [2, 8, 4, 6, 3];
 
-console.log(output);
+// console.log(output);
 
 function getDoubles(numbers) {
     const output = [];
@@ -9,15 +9,22 @@ function getDoubles(numbers) {
         output.push(doubled)
     }
     return output;
-} */
+}
 
-function doubleItOld(num) {
+ function doubleItOld(num) {
     return num * 2;
 }
 
 const doubleIt = num => num * 2;
 
-// const makedoubled = numbers.map(doubleIt);
+const makedoubled = numbers.map(doubleIt);
 const result = getDoubles(numbers);
 console.log(result);
 console.log(makedoubled);
+
+const makeDouble = numbers.map(doubleIt);
+const makeDoubleDirect = numbers.map(num => num * 2);
+const makeDouble2 = numbers.map(x => x * 2);
+// console.log(makeDoubleDirect);
+const fiveTimes = [1,2,3,4,5].map(x => x * 5);
+console.log(fiveTimes);
