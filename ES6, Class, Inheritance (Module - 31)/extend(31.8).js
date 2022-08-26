4a6c88c13f55ca4ba7b3fb6e825a3d964a3e7597
MrFM97 <shijawa@gmail.comm>
2022-08-26 17:58:53 +0600

    class extends to constructor, extend,super

--- ES6, Class, Inheritance (Module - 31)/extend(31.8).js
+++ ES6, Class, Inheritance (Module - 31)/extend(31.8).js
@@ -1,15 +1,70 @@
-name;
-designation = 'web learner'
-team = 'batch six'
-location;
-constructor(name, location){
-    this.name = name;
-    this.location = location;
+class TeamMember{
+    name;
+    location; //instantiate
+    constructor(name, location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    provideFeedback() {
+        console.log(`${this.name} thank you for your feedback`);
+    }
 }
-startSupportSession(time) {
-    console.log(`Join the support session at ${time}`)
+
+class Student extends TeamMember{
+    designation = 'web learner'
+    team = 'batch six'
+    constructor(name, location) {
+        super(name, location);
+    }
+    startSupportSession(time) {
+        console.log(`Join the support session at ${time}`)
+    }
+    createQuiz(module) {
+        console.log(`Please attend quiz for module ${module}`)
+    }
+
 }
-createQuiz(module){
-    console.log(`Please attend quiz for module ${module}`)
+
+class Developer extends TeamMember{
+    designation = 'web developer'
+    team = 'neptune'
+    tech;
+    constructor(name, location, tech) {
+        super(name, location);
+        this.tech = tech;
+    }
+    developFeature(feature) {
+        console.log(`Please develop the ${feature}`)
+    }
+    release(version) {
+        console.log(`Please release the version ${version}`)
+    }
+
 }
+
+class JobPlacement extends TeamMember{
+    designation = 'Job placement commandos'
+    team = 'Job placement'
+    region;
+    constructor(name, location, region) {
+        super(name, location);
+        this.region = region;
+    }
+    sendResume(resume) {
+        console.log(`Please send the resume at ${resume}`)
+    }
+    motivateStudent(motivate) {
+        console.log(`Please help the depressed student named ${motivate}`)
+    }
+
 }
+
+const alia = new Developer('Alia Bhatt', 'Dhaka', 'React');
+console.log(alia);
+alia.provideFeedback();
+const bipasha = new JobPlacement('bipasha basu', 'kolkata', 'india');
+console.log(bipasha);
+bipasha.sendResume('india');
+bipasha.motivateStudent('murta');
+bipasha.provideFeedback();
