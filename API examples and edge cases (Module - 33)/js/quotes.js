ef9621b6666734a12e114ea12a71e8f9431744eb
MrFM97 <shijawa@gmail.comm>
2022-08-28 20:25:00 +0600

    kanye rest API

---
+++ API examples and edge cases (Module - 33)/js/quotes.js
@@ -0,0 +1,10 @@
+const loadQuote = () => {
+    fetch('https://api.kanye.rest/')
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+const displayQuote = quote => {
+    const blockQuote = document.getElementById('quote');
+    blockQuote.innerText = quote.quote;
+}
