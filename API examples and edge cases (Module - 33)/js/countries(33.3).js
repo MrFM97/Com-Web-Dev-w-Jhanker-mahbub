d42ca38c8d71bec8cf887c1c4923c775766fd5ce
MrFM97 <shijawa@gmail.comm>
2022-08-29 17:35:16 +0600

    coutries, foreign country explore with API

--- API examples and edge cases (Module - 33)/js/countries(33.3).js
+++ API examples and edge cases (Module - 33)/js/countries(33.3).js
@@ -1,9 +1,10 @@
 const loadCountries = () => {
-    fetch('https://restcountries.com/#api-endpoints-v3-all')
+    fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => displayCountries(data));
 }
 const displayCountries = countries => {
+    console.log(countries);
     /* for (const country of countries) {
         console.log(country);
     } */
@@ -14,11 +15,28 @@ const displayCountries = countries => {
         
         countryDiv.innerHTML = `
         <h3>Name: ${country.name.common}</h3>
-        <p>Capital: ${country.capital ? country.length[0] : 'No capital'}</p>
-
-        ;`
+        <button onclick="loadCountryDetail('${country.cca2}')">Details</button>
+        `
         countriesContainer.appendChild(countryDiv)
     })
+}
+
+const loadCountryDetail = () => {
+    //https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log('get country');
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+const displayCountryDetail = country => {
+    const countryDetail = document.getElementById('countryDetail');
+    countryDetail.innerHTML = `
+    <h2>Details: ${country.name.common}</h2>
+    <img src="${country.flags.png}">
+    `
 }
 
 loadCountries();
