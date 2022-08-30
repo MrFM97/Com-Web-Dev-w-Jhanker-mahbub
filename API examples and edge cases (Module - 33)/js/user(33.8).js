bd654ac908136e00e5c3f39f7fde29e6fbaf690e
MrFM97 <shijawa@gmail.comm>
2022-08-30 12:36:32 +0600

    array with data, async, try catch error, await

---
+++ API examples and edge cases (Module - 33)/js/user(33.8).js
@@ -0,0 +1,25 @@
+const loadUserFetch = () => {
+    const url = `https://randomuser.me/api/?gender=female`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.results[0]))
+        .catch(error => console.log(error))
+}
+
+const loadUserAsync = async () => {
+    const url = `https://randomuser.me/apis/?gender=female`;
+    // error catching system
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayUser(data.results[0]); // console.log(data); ও লেখা যাবে
+    } catch (error) {
+        console.log(error);
+    }
+    // error catching system
+
+}
+
+const displayUser = user => {
+    console.log(user);
+}
