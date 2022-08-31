b220b7d0f58512be7cf924fe7ac9a720dcd1deb7
MrFM97 <shijawa@gmail.comm>
2022-08-31 22:27:43 +0600

    api call completed., search bar added

--- API recap with phone Hunter (Module - 34)/js/app.js
+++ API recap with phone Hunter (Module - 34)/js/app.js
@@ -1,4 +1,4 @@
-const loadPhones = async() => {
+const loadPhones = async () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
     const res = await fetch(url);
     const data = await res.json();
@@ -6,7 +6,22 @@ const loadPhones = async() => {
 }
 
 const displayPhones = phones => {
-    console.log(phones);
+    const phonesContainer = document.getElementById('phoneContainer');
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card p-5">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${phone.phone_name}</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+                content. This content is a little bit longer.</p>
+            </div>
+          </div>`;
+        phonesContainer.appendChild(phoneDiv);
+
+    });
 }
 
 loadPhones()
