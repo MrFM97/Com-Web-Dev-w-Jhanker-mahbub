5a9be132e19aef20194d2b5a7abae58f74d3bdb8
MrFM97 <shijawa@gmail.comm>
2022-09-02 01:54:48 +0600

    modal added

--- API recap with phone Hunter (Module - 34)/js/app.js
+++ API recap with phone Hunter (Module - 34)/js/app.js
@@ -36,7 +36,7 @@ const displayPhones = (phones, dataLimit) => {
               <h5 class="card-title">${phone.phone_name}</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
                 content. This content is a little bit longer.</p>
-                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
             </div>
           </div>`;
         phonesContainer.appendChild(phoneDiv);
@@ -62,11 +62,11 @@ document.getElementById('btnSearch').addEventListener('click', function () {
 });
 
 // search input field enter click handler
-document.getElementById('searchField').addEventListener('keypress', function (e) { 
+document.getElementById('searchField').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         processSearch(10);
     }
-    });    // search input field   enter keyup handler 
+});    // search input field   enter keyup handler 
 
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
@@ -89,9 +89,17 @@ const loadPhoneDetails = async id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     const res = await fetch(url);
     const data = await res.json(); // JSON.parse(response  .json() 
-    console.log(data.data); 
+    displayPhoneDetails(data.data);
 }
 
+const displayPhoneDetails = phone => { 
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetails = document.getElementById('phoneDetails');   
+    phoneDetails.innerHTML = `<p>release Date : ${phone.releaseDate ? phone.releaseDate : 'No release date found'}</p>
+    <p>Storage: ${phone.mainFeatures ? phone.mainFeatures.sorage : 'No storage information found'}</p>
+    <p>Others : ${phone.others ? phone.others.Bluetooth : 'No Bloototh devices found'}</p>`
+}
 
-
-loadPhones();
+loadPhones('apple');
