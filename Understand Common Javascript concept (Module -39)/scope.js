ddbb26852a0785ef9283f26bd17e132b3e1863ae
MrFM97 <shijawa@gmail.comm>
2022-09-13 23:24:45 +0600

    scope

---
+++ Understand Common Javascript concept (Module -39)/scope.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+    const total = a + b;
+    console.log(a, b);
+    if (b > 5) {
+        const sum = 25 + a + b;
+    }
+    return total;
+}
+// console.log(total);
+add(1, 2);
